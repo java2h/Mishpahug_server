f0fe77095a90305e088411d5c1795921c188297a
mrcolombo1985 <45918270+mrcolombo1985@users.noreply.github.com>
2019-11-25 21:39:00 +0200

    пвапвава

--- MishpahugHaifa/src/main/java/application/entities/data/DeviceEntity.java
+++ MishpahugHaifa/src/main/java/application/entities/data/DeviceEntity.java
@@ -1,4 +1,40 @@
 package application.entities.data;
 
+        import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+        import lombok.*;
+        import lombok.extern.slf4j.Slf4j;
+
+        import javax.persistence.*;
+
+@Entity
+@Table(name = "device")
+@ToString(exclude = {"iparduino", "port", "description"})
+@EqualsAndHashCode(of = {"name"})
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Slf4j
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class DeviceEntity {
+
+    @Id
+    @Column(name = "Id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "iparduino")
+    private String ipArduino;
+
+    @Column(name = "port")
+    private Integer port;
+
+
 }
--- MishpahugHaifa/src/main/java/application/entities/data/DeviceTypeEntity.java
+++ MishpahugHaifa/src/main/java/application/entities/data/DeviceTypeEntity.java
@@ -1,4 +1,29 @@
 package application.entities.data;
 
-public class ВумшсуЕнзу {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "devicetype")
+@ToString(exclude = {"name"})
+@EqualsAndHashCode(of = {"name"})
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Slf4j
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class DeviceTypeEntity {
+
+    @Id
+    @Column(name = "Id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
 }
