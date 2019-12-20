87286b3014b3018ba5c2b496673884d64b41ff11
mrcolombo1985@gmail.com <mrcolombo1985>
2019-12-20 22:57:58 +0200

    продолжение

--- javaserverclient/src/main/java/application/GuiApplication.java
+++ javaserverclient/src/main/java/application/GuiApplication.java
@@ -1,4 +1,10 @@
 package application;
 
 public class GuiApplication {
+    public static void main(String[] args) {
+        MainForm mainForm = new MainForm();
+        mainForm.setSize(1024,768);
+        mainForm.pack();
+        mainForm.setVisible(true);
+    }
 }
--- javaserverclient/src/main/java/application/MainForm.java
+++ javaserverclient/src/main/java/application/MainForm.java
@@ -1,4 +1,51 @@
 package application;
 
-public class MainForm {
+import javax.swing.*;
+
+public class MainForm extends JFrame {
+    private JTabbedPane mainPanel;
+    private JPanel panel1;
+    private JTabbedPane tabbedPane2;
+    private JTabbedPane tabbedPane3;
+    private JTabbedPane tabbedPane4;
+    private JTabbedPane tabbedPane5;
+    private JTabbedPane tabbedPane6;
+    private JTabbedPane tabbedPane7;
+    private JTabbedPane tabbedPane8;
+    private JList list1;
+    private JList list2;
+    private JList list3;
+    private JList list4;
+    private JList list5;
+    private JList list6;
+    private JList list7;
+    private JList list8;
+    private JList list9;
+    private JList list10;
+    private JList list11;
+    private JList list12;
+    private JList list13;
+    private JList list14;
+    private JList list15;
+    private JList list16;
+    private JList list17;
+    private JList list18;
+    private JList list19;
+    private JList list20;
+    private JList list21;
+    private JList list22;
+    private JList list23;
+    private JList list24;
+    private JList list25;
+    private JList list26;
+    private JList list27;
+    private JList list28;
+    private JList list29;
+    private JList list30;
+    private JList list31;
+    private JList list32;
+    private JPanel devicePanel;
+    private JPanel aboutPanel;
+    private JPanel planePanel;
+    private JPanel optionPanel;
 }
